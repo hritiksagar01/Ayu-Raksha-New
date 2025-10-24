02501f8061740e2a6f545a5ff7c067242571fcb6
hritiksagar01 <143049203hritiksagar01@users.noreply.github.com>
2025-10-24 16:15:14 +0530

    test

--- src/constants/translations.ts
+++ src/constants/translations.ts
@@ -13,7 +13,7 @@ export const translations = {
   
   // Hero Section
   welcomeText: {
-    English: 'Welcome to Ayu Raksha',
+    English: 'Wwwwelcome to Ayu Raksha',
     Hindi: 'आयु रक्षा में आपका स्वागत है',
   },
   tagline: {
